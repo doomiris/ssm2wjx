1631dea5c0f5885ec50858ad3df0edc5fa951b8c
Dominic Lan <doomiris@outlook.com>
2020-07-15 09:41:59 -0500

    由于autojs_sdk.click_item() 在微信界面失效. 修改了相应部分代码.

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const getToday = (sep) => {
 const storage = storages.create('wjx');
 let myname = storage.get('name');
 if (!myname) {
-    myname = dialogs.prompt("请输入孩子的真实姓名", "");
+    myname = dialogs.prompt('请输入孩子的真实姓名', '');
     if (myname.length>0) storage.put('name', myname);
 }
 
@@ -28,7 +28,7 @@ files.ensureDir(capimg);
 console.log(capimg);
 
 if (files.exists(capimg)) {
-    toastLog("今天已经上传了");
+    toastLog('今天已经上传了');
     exit();
 }
 const cle = (e) => {
@@ -44,11 +44,29 @@ const swipe_to_click = function(text, upward) {
 };
 let main = function(){
     sleep(3000);
+    id('cns').text('微信').findOne().parent().parent().click();
+    swipe_down();
+    sleep(2000);
+    id('gam').text('随申办').findOne().parent().click();
+    className('android.view.View').text('随申码').findOne().click()
+    className('android.view.View').text('亲属随申码').findOne().click()
+    className('android.view.View').text(myname + '的随申码').waitFor()
+    let e = text('查看').findOnce( 0 + 1 );
+    cle(e);
+    className('android.view.View').text('绿色').depth(22).waitFor();
+    sleep(1000);
+    captureScreen(capimg);
+    sleep(100);
+    start_app(lmain, '微信', '把截图上传到问卷星', false, true);
+};
+let main_bak = function(){
+    sleep(3000);
     id('cns').text('我').findOne().parent().parent().click();
     click_item('支付');
+    click_item('随申码');
     click_item('防疫健康码');
     click_item('查看防疫健康码');
-    click_item('亲属随申码');
+    click_item('亲属随申码');    
     wait_for(myname + '的随申码');
     let e = text('查看').findOnce( 0 + 1 );
     cle(e);
@@ -58,16 +76,18 @@ let main = function(){
     sleep(100);
     start_app(subaction, '微信', '把截图上传到问卷星', false, true);
 };
-let subaction = function(){
+let lmain = function(){
     sleep(2000);
     id('cns').text('我').findOne().parent().parent().click();
-    click_item('收藏');
+    sleep(1000);
+    let sc = className('android.widget.TextView').text('收藏').depth(20).findOne();
+    cle(sc);
     swipe_to_click('政海幼儿园幼儿每日健康随申码收集表');
     sleep(7*1000);
     click_item('请选择日期',[250,125]);
     wait_for('确定');
     click_item('确定');
-    input(1,myname);   
+    input(1,myname); 
     input(2,ctt);
     swipe_to_click('上海');
     click_item('选择文件');
